2ff4e53557d13b47a5806a007f62105a8c366580
devonline <devonline.academy@gmail.com>
2019-04-25 15:58:35 +0300

    Base exception classes

--- code/src/main/java/academy/devonline/javamm/code/exception/ConfigException.java
+++ code/src/main/java/academy/devonline/javamm/code/exception/ConfigException.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,12 +14,22 @@
  * limitations under the License.
  */
 
+package academy.devonline.javamm.code.exception;
+
+import static java.util.Objects.requireNonNull;
+
 /**
  * @author devonline
  * @link http://devonline.academy/javamm
  */
-module javamm.code {
-    exports academy.devonline.javamm.code.fragment;
-    exports academy.devonline.javamm.code.exception;
-    exports academy.devonline.javamm.code.fragment.operation;
+public final class ConfigException extends RuntimeException {
+
+    public ConfigException(final String message) {
+        super(requireNonNull(message));
+    }
+
+    public ConfigException(final String message,
+                           final Throwable cause) {
+        super(requireNonNull(message), requireNonNull(cause));
+    }
 }
