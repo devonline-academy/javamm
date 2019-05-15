286c193ea9e289a8b4387f2124613ac85879793d
devonline <devonline.academy@gmail.com>
2019-05-15 15:56:58 +0300

    PairedTokensHelperImpl

--- ide/src/main/java/academy/devonline/javamm/ide/component/impl/PairedTokensHelperImpl.java
+++ ide/src/main/java/academy/devonline/javamm/ide/component/impl/PairedTokensHelperImpl.java
@@ -19,19 +19,34 @@ package academy.devonline.javamm.ide.component.impl;
 import academy.devonline.javamm.ide.component.PairedTokensHelper;
 import org.fxmisc.richtext.CodeArea;
 
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 /**
  * @author devonline
  * @link http://devonline.academy/javamm
  */
 public class PairedTokensHelperImpl implements PairedTokensHelper {
 
+    private final Map<String, String> map = Map.ofEntries(
+        entry("[", "]"),
+        entry("(", ")"),
+        entry("{", "}"),
+        entry("'", "'"),
+        entry("\"", "\"")
+    );
+
     @Override
     public boolean isPairedToken(final String character) {
-        return false;
+        return map.containsKey(character);
     }
 
     @Override
     public void insertPairedToken(final CodeArea codeArea, final String character) {
-
+        final String token = map.get(character);
+        final int caretPosition = codeArea.getCaretPosition();
+        codeArea.insertText(caretPosition, token);
+        codeArea.moveTo(caretPosition);
     }
 }
